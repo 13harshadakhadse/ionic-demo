060e0dac76e7a2e2102e248ce4abb31dc422961f
bambarkar1993 <bambarkar1993@gmail.com>
2023-08-19 12:17:09 +0530

    commit all

--- src/app/otp/otp.component.ts
+++ src/app/otp/otp.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/service/user.service';
+import { SubSink } from 'subsink/dist/subsink';
 
 @Component({
   selector: 'app-otp',
@@ -7,8 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OtpComponent  implements OnInit {
 
-  constructor() { }
+  private sub = new SubSink();
+
+  constructor(
+    private route: Router, 
+    private _userService: UserService
+  ) { }
 
   ngOnInit() {}
+  
+
+  moveFocus(event : any, nextElement : any, previousElement : any) {
+    console.log(event.keyCode);
+    if (event.keyCode === 8 && previousElement) {
+      previousElement.setFocus();
+    } else if (event.keyCode >= 48 && event.keyCode <= 57) {
+      if (nextElement) {
+        nextElement.setFocus();
+      }
+    } else if (event.keyCode >= 96 && event.keyCode <= 105) {
+      if (nextElement) {
+        nextElement.setFocus();
+      }
+    } else {
+      event.path[0].value = '';
+    }
+  }
+
+  goTODashboard()
+  {
+    this.route.navigate(['/home']);
+  }
 
 }
